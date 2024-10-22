15ced10cebaeaed9adaf093a42d5a13ae196974c
Ritesh <officialriteshwork@gmail.com>
2024-10-22 10:41:07 +0530

    kuch toh hua

---
+++ 146.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+for (int i = 5; i >=1 ; i--)
+{
+    for (int j = i; j >=1; j--)
+    {
+        printf(" ");
+    }
+    for (int k = 0; k < count; k++)
+    {
+        /* code */
+    }
+    
+    printf("\n");
+}
+
+}
