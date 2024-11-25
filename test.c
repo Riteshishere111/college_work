8c2ff321bac0c388d609509737213304ad1014c4
Ritesh <officialriteshwork@gmail.com>
2024-11-26 05:15:55 +0530

    update

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+float decimal;
+char a;
+num=2;
+decimal=3.21;
+a = 'd';
+printf("example of simple value is =%d\n example of Decimal value is =%f\n example of character value is=%c",num,decimal,a);
+}
