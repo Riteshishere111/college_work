e607406af5b58f374562535bcae80687194f6486
Ritesh <113716363+Riteshishere111@users.noreply.github.com>
2024-11-26 07:26:54 +0530

    Update and rename que23.c to 23.c

--- 23.c
+++ 23.c
@@ -2,12 +2,12 @@
 #include <conio.h>
 #include <math.h>
 
-int main()
+void main()
 {
     int l,b,h,v,TSA,d;
 printf("\n Enter the length = ");
 scanf("%f",&l);
-printf("\n Enter the breath = ");
+printf("\n Enter the breadth = ");
 scanf("%f",&b);
 printf("\n Enter the height = ");
 scanf("%f",&h);
@@ -15,5 +15,4 @@ scanf("%f",&h);
  TSA=2*((l*b)+(b*h)+(h*l));
  d=sqrt((l*l)+(b*b)+(h*h));
  printf("\n value of v=%f,\n value of TSA=%f,\n Value of D=%f",v,TSA,d);
-    return 0;
 }
