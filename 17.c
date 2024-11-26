10be64fc6600a349515a62ca5f7e3e3e97ff00fb
Its Tiwari <addhyant85@gmail.com>
2024-11-26 07:17:53 +0530

    17.c

--- 17.c
+++ 17.c
@@ -1,15 +1,13 @@
+// Write a program to input a and b as integers and print it in from a/b.
+
 #include <stdio.h>
 #include <conio.h>
 #include<math.h>
-int main()
+void main()
 {
     int l,b,a,p;
-<<<<<<< HEAD
     printf("\n Enter the length = ");
-=======
-
      printf("\n Enter the length = ");
->>>>>>> 3ca2eebbd54b3915416b9a195354a70359dec7bc
     scanf("%d",&l);
     printf("\n Enter the Wridth = ");
     scanf("%d",&b);;
@@ -17,5 +15,4 @@ int main()
     p=2*(l+b);
     printf("\n Area of Rectangle = %d",a);
     printf("\n Perimeter of Rectangle = %d",p);
-    return 0;
 }
