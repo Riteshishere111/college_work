be5070ffa1648d9a70758c94ca4a3cc8a60ceda4
Ritesh <113716363+Riteshishere111@users.noreply.github.com>
2024-11-26 07:15:58 +0530

    Update 15.c

--- 15.c
+++ 15.c
@@ -12,8 +12,8 @@ int main()
         scanf("%d",&a);
     printf("\n Enter a decimal number =");
         scanf("%f",&b);
-    printf("\n Enter a charater =");
+    printf("\n Enter a character =");
         scanf("%c",&c);
-        printf("\n integer value=%d\n Decimal value=%f \n Charater value=%c",a,b,c);
+        printf("\n integer value=%d\n Decimal value=%f \n Character value=%c",a,b,c);
     return 0;
 }
