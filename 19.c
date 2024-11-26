e9f5d20f9faa7fcabc29dca324be496a7587218a
Ritesh <113716363+Riteshishere111@users.noreply.github.com>
2024-11-26 07:19:57 +0530

    Update and rename que19.c to 19.c

--- 19.c
+++ 19.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <conio.h>
 #include<math.h>
-int main()
+void main()
 {
     float s,a;
     printf("\n Enter the length of the square = ");
     scanf("%f",&s);
     a=s*s;
     printf("\n Area of the square =%f",a);
-    return 0;
 }
