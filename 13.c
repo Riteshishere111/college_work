830e281bec752f28069110a687073456723c9f80
Ritesh <113716363+Riteshishere111@users.noreply.github.com>
2024-11-26 07:18:21 +0530

    Update and rename que13.c to 13.c

--- 13.c
+++ 13.c
@@ -3,11 +3,10 @@
 #include <stdio.h>
 #include <conio.h>
 
-int main()
+void main()
 {
     char k;
-    printf("\n Enter the charater = ");
+    printf("\n Enter the character = ");
     scanf("%c",&k);
     printf("\n The value of a =%c",k);
-    return 0;
 }
