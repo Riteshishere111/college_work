f94bc49680a89e8037fcf9ec8ff113f5651ed3f1
Ritesh <113716363+Riteshishere111@users.noreply.github.com>
2024-11-26 07:08:21 +0530

    Update and rename que10.c to 10.c

--- 10.c
+++ 10.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <conio.h>
 
-int main()
+void main()
 {
     float a,b;
     a=55.99;
     b=2080.36;
         printf("\n the value of a=%f",a);
         printf("\n the value of b=%f",b);    
-    return 0;
 }
