26f9caf2fb78f2888ff8a87b6e2fb30481591c5a
Ritesh <officialriteshwork@gmail.com>
2024-11-27 20:16:36 +0530

    update

--- 207.c
+++ 207.c
@@ -1,1 +1,15 @@
-// INCOMLETE
+// WAP to input a number and check it is perfect number or not using function with return value 
+#include<stdio.h>
+#include<conio.h>
+int check(int);
+void main()
+{
+int n;
+printf("Enter a number: ");
+scanf("%d",&n);
+printf("%d",check(n));
+
+}
+int check(int){
+    
+}
